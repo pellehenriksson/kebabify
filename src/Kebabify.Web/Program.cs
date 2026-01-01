e532cbcfacbe1dc861ed7d41d0abcc90fe1f4c33
Pelle Henriksson <pelle.henriksson@gmail.com>
2026-01-01 11:44:51 +0100

    added swagger support

--- src/Kebabify.Web/Program.cs
+++ src/Kebabify.Web/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Kebabify.Web.Common;
 using Kebabify.Web.Domain.Commands;
 using Kebabify.Web.Domain.Services;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 namespace Kebabify.Web
 {
@@ -17,12 +19,34 @@ namespace Kebabify.Web
             builder.Services.AddTransient<KebabService, KebabService>();
             builder.Services.AddSingleton(new SystemClock());
 
+            // OpenAPI / Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Kebabify API", Version = "v1" });
+
+                // Include XML comments if available
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                if (File.Exists(xmlPath))
+                {
+                    options.IncludeXmlComments(xmlPath);
+                }
+            });
+
             var app = builder.Build();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
+
+            // Swagger UI (enabled in Development and Staging)
+            if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kebabify API v1"));
+            }
 
             app.MapRazorPages();
             app.MapControllers();
